fd3abb2fd0c0e59b4876bb6085946c1439d63759
Wenzelgood <slavazylneko@gmail.com>
2022-08-01 18:53:27 +0300

    Highlight Districts Function Added

--- VtM Katowice Map/js/main.js
+++ VtM Katowice Map/js/main.js
@@ -1,25 +1,47 @@
 let map = L.map('map').setView([50.270908, 19.039993], 12);
 
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    maxZoom: 25,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+async function dataFetch (url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  console.dir(data);
+  return data;
+}
+
+let geoKatowice = dataFetch('js/map-data.geojson').then(data => {
+  let geojsonData = L.geoJSON(data, {
+    style: style,
+    onEachFeature: function (feature, layer) {
+      if (feature.properties && feature.properties.title) {
+        layer.bindPopup(feature.properties.title);
+      } 
+      if (layer.feature.geometry.type === 'Polygon') {
+        layer.on({
+          mouseover: e => {
+            layer.setStyle({
+              weight: 2,
+              color: '#fff'
+            })
+            if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+              layer.bringToFront();
+            }
+          },
+          mouseout: e => {
+            geojsonData.resetStyle(e.target)
+          }
+        });
+      }
+    }
+  }).addTo(map)
+});
+
 function style(feature) {
   return {
       fillColor: feature.properties.fill,
       color: feature.properties.fill
   };
 }
-
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-    maxZoom: 25,
-    attribution: '© OpenStreetMap'
-}).addTo(map);
-
-
-let geoJsonData = fetch('js/map-data.geojson')
-  .then(res => res.json())
-  .then(data => L.geoJSON(data, {
-    style: style,
-    onEachFeature: function (feature, layer) {
-      if (feature.properties && feature.properties.title) {
-        layer.bindPopup(feature.properties.title);
-      }
-    }
-  }).addTo(map));
